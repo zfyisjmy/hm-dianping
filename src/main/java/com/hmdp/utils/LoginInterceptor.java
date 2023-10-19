13ffec880c9e553c6271589efd8ceb5ad15ae55b
zfy <2721192982@qq.com>
2023-10-19 15:57:37 +0800

    first commint

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在
+        if (user == null){
+            response.setStatus(401);
+            return false;
+        }
+        //4.存在，保存用户信息到TreadLocal中
+        UserHolder.saveUser((User) user);
+        //5.放行
+        return true;
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
